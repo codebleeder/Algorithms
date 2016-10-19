40e2de5eba085e028f8c31cd6f5959c67921b189
codebleeder <sharad.shivmath@gmail.com>
2016-10-19 07:04:19 -0400

    Chapter 1 problems 8, and 9

---
+++ src/algo/Util.java
@@ -0,0 +1,11 @@
+package algo;
+
+/**
+ * Created by Sharad on 10/19/2016.
+ */
+public class Util {
+    public static void printMatrix(int[][] m){
+        for (int i=0; i<m.length; i++)
+            System.out.println(java.util.Arrays.toString(m[i]));
+    }
+}
