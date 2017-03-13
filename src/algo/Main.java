bf7eba082e616e813de80cdfa035d3b111867611
codebleeder <sharad.shivmath@gmail.com>
2017-03-13 07:38:52 -0400

    Chapter 3: Stacks, and Queues

--- src/algo/Main.java
+++ src/algo/Main.java
@@ -3,11 +3,15 @@ package algo;
 import algo.ctci.ch2.TestCh2;
 import algo.ctci.ch2.TestLinkedList;
 import algo.ctci.ch2.TestMisc;
+import algo.ctci.ch3.TestStack;
+import algo.ctci.ch3.TestCh3;
 
 public class Main {
 
     public static void main(String[] args) {
-       TestCh2 t = new TestCh2();
-        t.testLoopDetection8();
+       TestCh3 t = new TestCh3();
+        boolean b = t.testFixedMultiStack();
+        if (b == true) System.out.println("pass!");
+        else System.out.println("fail!"); 
     }
 }
