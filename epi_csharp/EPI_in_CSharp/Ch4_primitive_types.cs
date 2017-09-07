7c62a9c978f5202cdcd03c2b561b5e2d76ca5753
codebleeder <sharad.shivmath@gmail.com>
2017-09-06 23:14:56 -0400

    added new projects

---
+++ epi_csharp/EPI_in_CSharp/Ch4_primitive_types.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPI_in_CSharp
+{
+    public class Ch4_primitive_types
+    {
+        public static int countBits(int x)
+        {
+            int count = 0; 
+            while (x != 0)
+            {
+                count += (x & 1);
+                x = x >> 1; 
+            }
+            return count; 
+        }
+        
+        public static bool parity(int x)
+        {
+            bool result = false; 
+            while (x != 0)
+            {
+                result ^= Convert.ToBoolean(x & 1);
+                x = x >> 1;
+            }
+            return result; 
+        }
+
+        public static void testParity()
+        {
+            System.Console.WriteLine("parity 4 = " + parity(4));
+            System.Console.WriteLine("parity 5 = " + parity(5));
+        }
+
+        public static bool parity2(int x)
+        {
+            
+            bool result = false; 
+            while (x != 0)
+            {
+                result ^= true;
+                x = x & (x - 1);
+            }
+            return result; 
+        }
+
+        public static void testParity2()
+        {
+            System.Console.WriteLine("parity 4 = " + parity2(4));
+            System.Console.WriteLine("parity 5 = " + parity2(5));
+        }
+    }
+}
---
+++ epi_csharp/EPI_in_CSharp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EPI_in_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ch4_primitive_types.testParity();
+            Ch4_primitive_types.testParity2();
+        }
+    }
+}
