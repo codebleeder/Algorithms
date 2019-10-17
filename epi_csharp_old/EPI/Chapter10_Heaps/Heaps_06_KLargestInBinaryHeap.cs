104133f236ce71aac9bc776636769a6b910299f4
Sharad Shivmath <sharad.shivmath@gmail.com>
2019-10-17 13:21:44 +0530

    Heaps: problems 6 & 7

---
+++ epi_csharp_old/EPI/Chapter10_Heaps/Heaps_06_KLargestInBinaryHeap.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPI.Chapter10_Heaps
+{
+    public static class Heaps_06_KLargestInBinaryHeap
+    {
+        public static List<int> KLargestInBinaryHeap(List<int> heap, int k)
+        {
+            if (k <= 0)
+            {
+                return new List<int>();
+            }
+            var candidateHeapMax = new MaxPriorityQueue<HeapEntry>();
+            var res = new List<int>();
+            candidateHeapMax.Enqueue(new HeapEntry(0, heap[0]));
+            for ( var i = 0; i < k; i++ )
+            {
+                var candidateIndex = candidateHeapMax.Peek().Index;
+                res.Add(candidateHeapMax.Dequeue().Value);
+
+                var leftChildIndex = candidateIndex * 2 + 1;
+                if (leftChildIndex < heap.Count)
+                {
+                    candidateHeapMax.Enqueue(new HeapEntry(leftChildIndex, heap[leftChildIndex]));
+                }
+                var rightChildIndex = candidateIndex * 2 + 2;
+                if (rightChildIndex < heap.Count)
+                {
+                    candidateHeapMax.Enqueue(new HeapEntry(rightChildIndex, heap[rightChildIndex]));
+                }
+            }
+            return res;
+        }
+        class HeapEntry: IComparable<HeapEntry>
+        {
+            public int Index { get; set; }
+            public int Value { get; set; }
+            public HeapEntry(int index, int value)
+            {
+                Index = index;
+                Value = value;
+            }
+
+            public int CompareTo(HeapEntry other)
+            {
+                return Value.CompareTo(other.Value);
+            }
+        }
+        public static void Test()
+        {
+            var heap = new List<int> { 561, 314, 401, 28, 156, 359, 271, 11, 3 };
+            var res = KLargestInBinaryHeap(heap, 4);
+            Utilities.PrintCollection<int>(res.GetEnumerator());
+        }
+    }
+}
---
+++ epi_csharp_old/EPI/Chapter10_Heaps/Heaps_07_Stack.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPI.Chapter10_Heaps
+{
+    public static class Heaps_07_Stack
+    {
+        public static void Test()
+        {
+            var values = new List<int> { 23, 233, 12, 64, 74 };
+            var stack = new Stack<int>();
+            var stackUsingHeap = new StackUsingHeap<int>();
+            foreach(var v in values)
+            {
+                stack.Push(v);
+                stackUsingHeap.Push(v);
+            }
+            for(var i = 0; i < values.Count; i++)
+            {
+                var pop1 = stack.Pop();
+                var pop2 = stackUsingHeap.Pop();
+                var testRes = pop1 == pop2 ? "pass" : "fail";
+                Console.WriteLine($"stack pop: {pop1}  using heap: {pop2}  result: {testRes}");
+            }
+        }
+    }
+    public class HeapEntryWithIndex<T>: IComparable<HeapEntryWithIndex<T>>
+    {
+        public int Index { get; set; }
+        public T Value { get; set; }
+        public HeapEntryWithIndex(T value, int index)
+        {
+            Value = value;
+            Index = index;
+        }
+
+        public int CompareTo(HeapEntryWithIndex<T> other)
+        {
+            return Index.CompareTo(other.Index);
+        }
+    }
+    public class StackUsingHeap<T>
+    {
+        public MaxPriorityQueue<HeapEntryWithIndex<T>> maxHeap { get; set; }
+        public int Count { get { return maxHeap.Count; }  }
+        public StackUsingHeap()
+        {
+            maxHeap = new MaxPriorityQueue<HeapEntryWithIndex<T>>();
+        }
+        public void Push(T data)
+        {
+            maxHeap.Enqueue(new HeapEntryWithIndex<T>(data, Count));
+        }
+        public T Pop()
+        {
+            return maxHeap.Dequeue().Value;
+        }
+        public T Peek()
+        {
+            return maxHeap.Peek().Value;
+        }
+    }
+}
